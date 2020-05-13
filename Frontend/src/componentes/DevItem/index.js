f2b2b9aaf31112a5e2f7dc13e31f787cfb90c94a
Ailsoncf <ailson_cf@yahoo.com.br>
2020-05-13 14:57:57 +1200

    Frontend

--- Frontend/src/componentes/DevItem/index.js
+++ Frontend/src/componentes/DevItem/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import './styles.css'
+import { FiTrash2 } from 'react-icons/fi'
+
 
 function DevItem({ dev }){
     return(
-        <li className='dev-item'>
-        <header>
-          <img src={dev.avatar_url} alt={dev.name} />
-          <div className='user-info'>
-            <strong>{dev.name}</strong>
-            <span>{dev.techs.join(', ')}</span>
-          </div>
-        </header>
-        <p>{dev.bio}</p>
-        <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil do Github</a>
+      <li className='dev-item'>
+      <header>
+        <img src={dev.avatar_url} alt={dev.name} />
+        <div className='user-info'>
+          <strong>{dev.name}</strong>
+          <span>{dev.techs.join(', ')}</span>
+        </div>
+        <button type="button">
+          <FiTrash2 size={18} color="#a8a8b3"/>
+        </button>
+      </header>
+      <p>{dev.bio}</p>
+      <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil do Github</a>
       </li> 
     )
 }
