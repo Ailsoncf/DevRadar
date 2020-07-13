14a752fbf121a1d959d72bc58a743bdea1368c68
Ailsoncf <ailson_cf@yahoo.com.br>
2020-07-13 22:36:45 +1200

    Frontend

--- Frontend/src/componentes/DevItem/index.js
+++ Frontend/src/componentes/DevItem/index.js
@@ -12,9 +12,11 @@ function DevItem({ dev }){
           <strong>{dev.name}</strong>
           <span>{dev.techs.join(', ')}</span>
         </div>
+        <div className='trash-button'>
         <button type="button">
           <FiTrash2 size={18} color="#a8a8b3"/>
         </button>
+        </div>
       </header>
       <p>{dev.bio}</p>
       <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil do Github</a>
